455861283fcf69ca35b6c126695a0059d3d96a3c
adrielebeatriz <85469805+adrielebeatriz@users.noreply.github.com>
2021-10-09 07:49:08 -0300

    lista 1 completa

--- lista1/com/dbc/exercicio6.java
+++ lista1/com/dbc/exercicio6.java
@@ -5,8 +5,58 @@ import java.util.Scanner;
 public class exercicio6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("diga uma palavra para ser traduzida");
 
+        System.out.println("Diga qual palavra será traduzida: ");
+        String word = input.next();
+        word = word.toUpperCase();
+
+        switch (word) {
+            case "CACHORRO":
+                System.out.println("A tradução da palavra " + word + " em inglês é Dog");
+                break;
+
+            case "DOG":
+                System.out.println("A tradução da palavra " + word + " em português é Cachorro");
+                break;
+
+            case "TEMPO":
+                System.out.println("A tradução da palavra " + word + " em inglês é Time");
+                break;
+
+            case "TIME":
+                System.out.println("A tradução da palavra " + word + " em português é Tempo");
+                break;
+
+            case "AMOR":
+                System.out.println("A tradução da palavra " + word + " em inglês é Love");
+                break;
+
+            case "LOVE":
+                System.out.println("A tradução da palavra " + word + " em português é Amor");
+                break;
+
+            case "CIDADE":
+                System.out.println("A tradução da palavra  " + word + " em inglês é City");
+                break;
+
+            case "CITY":
+                System.out.println("A tradução da palavra  " + word + " em portugês é Cidade");
+                break;
+
+            case "FELIZ":
+                System.out.println("A tradução da palavra  " + word + " em inglês é Happy");
+                break;
+
+            case "HAPPY":
+                System.out.println("A tradução da palavra" + word + "em português é: Feliz  " );
+                break;
+
+            default:
+                System.out.println("Palavra invalida");
+                break;
+
+        }
+    }
 
     }
 }
