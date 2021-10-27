aaa6b893aea1d34882592429b8b6ee97b59fed47
Adriele Beatriz Santos <adriele.beatriz98@outlook.com>
2021-10-27 07:31:52 -0300

    homework2

---
+++ modulo 3/pessoaapi/pessoaapi/src/main/java/com/dbc/pessoaapi/controller/ContatoController.java
@@ -0,0 +1,45 @@
+package com.dbc.pessoaapi.controller;
+
+import com.dbc.pessoaapi.entity.Contato;
+import com.dbc.pessoaapi.service.ContatoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/contato")
+public class ContatoController {
+
+    private ContatoService contatoService;
+
+    public ContatoController() {
+        contatoService = new ContatoService();
+    }
+
+    @PostMapping ("/{idPessoa}")
+    public Contato create(@PathVariable ("idPessoa") Integer id,@RequestBody Contato contato) {
+        return contatoService.create(id,contato);
+    }
+
+    @GetMapping
+    public List<Contato> list() {
+        return contatoService.list();
+    }
+
+    @GetMapping("/byidpessoa")
+    public List<Contato> listByIdPessoa(@RequestParam("idPessoa") Integer id) {
+        return contatoService.listByIdPessoa(id);
+    }
+
+
+    @PutMapping("/{idContato}")
+    public Contato update(@PathVariable("idContato") Integer id,
+                          @RequestBody Contato contatoAtualizar) throws Exception {
+        return contatoService.update(id, contatoAtualizar);
+    }
+
+    @DeleteMapping("/{idContato}")
+    public void delete(@PathVariable("idContato") Integer id) throws Exception {
+        contatoService.delete(id);
+    }
+    }
+
---
+++ modulo 3/pessoaapi/pessoaapi/src/main/java/com/dbc/pessoaapi/repository/ContatoRepository.java
@@ -0,0 +1,66 @@
+package com.dbc.pessoaapi.repository;
+
+
+
+import com.dbc.pessoaapi.entity.Contato;
+import com.dbc.pessoaapi.entity.Pessoa;
+import com.dbc.pessoaapi.entity.TipoContato;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class ContatoRepository {
+
+    private AtomicInteger COUNTER = new AtomicInteger();
+    private static List<Contato> listaContatos = new ArrayList<>();
+    private AtomicInteger COUNTER2 = new AtomicInteger();
+
+    public ContatoRepository() {
+
+        listaContatos.add(new Contato(COUNTER.incrementAndGet(), COUNTER2.incrementAndGet(), TipoContato.COMERCIAL, "71987130989", "trabalho"));
+        listaContatos.add(new Contato(COUNTER.incrementAndGet(), COUNTER2.incrementAndGet(), TipoContato.RESIDENCIAL, "7182759675", "casa"));
+        listaContatos.add(new Contato(COUNTER.incrementAndGet(), COUNTER2.incrementAndGet(), TipoContato.COMERCIAL, "71986759796", "trabalho dois"));
+    }
+
+    public Contato create( Integer IdPessoa,Contato contato) {
+        contato.setIdPessoa(IdPessoa);
+        contato.setIdContato(COUNTER.incrementAndGet());
+        listaContatos.add(contato);
+
+        return contato;
+    }
+
+
+    public List<Contato> list() {
+        return listaContatos;
+    }
+
+    public List<Contato> listByIdPessoa(Integer id) {
+        return listaContatos.stream()
+                .filter(contato -> contato.getIdPessoa().equals(id))
+                .collect(Collectors.toList());
+    }
+
+    public Contato update(Integer id, Contato contatoAtualizar) throws Exception {
+        Contato contatobackup = listaContatos.stream()
+                .filter(contato -> contato.getIdContato().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new Exception("Contato não  foi encontrado"));
+        contatobackup.setIdPessoa(contatoAtualizar.getIdPessoa());
+        contatobackup.setTipoContato(contatoAtualizar.getTipoContato());
+        contatobackup.setNumero(contatoAtualizar.getNumero());
+        contatobackup.setDescricao(contatoAtualizar.getDescricao());
+        return contatobackup;
+    }
+
+    public void delete(Integer id) throws Exception {
+        Contato contatobackup = listaContatos.stream()
+                .filter(contato -> contato.getIdContato().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new Exception("Contato não encontrado"));
+        listaContatos.remove(contatobackup);
+    }
+}
