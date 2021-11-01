5348f6ada26d78a23454f9510ca770594cb27073
Adriele Beatriz Santos <adriele.beatriz98@outlook.com>
2021-11-01 10:52:00 -0300

    update contato dto

--- modulo 3/pessoaapi/pessoaapi/src/main/java/com/dbc/pessoaapi/service/ContatoService.java
+++ modulo 3/pessoaapi/pessoaapi/src/main/java/com/dbc/pessoaapi/service/ContatoService.java
@@ -1,5 +1,4 @@
 package com.dbc.pessoaapi.service;
-
 import com.dbc.pessoaapi.DTO.ContatoCreateDTO;
 import com.dbc.pessoaapi.DTO.ContatoDTO;
 import com.dbc.pessoaapi.entity.ContatoEntity;
@@ -7,59 +6,57 @@ import com.dbc.pessoaapi.exceptions.RegraDeNegocioException;
 import com.dbc.pessoaapi.repository.ContatoRepository;
 import com.dbc.pessoaapi.repository.PessoaRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@AllArgsConstructor
 public class ContatoService {
-
     private final ContatoRepository contatoRepository;
     private final PessoaRepository pessoaRepository;
     private final ObjectMapper objectMapper;
 
 
-    public ContatoDTO create(Integer idPessoa, ContatoCreateDTO contatoCreateDTO) throws RegraDeNegocioException {
-        pessoaRepository.getIdById(idPessoa);
-        contatoCreateDTO.setIdPessoa(idPessoa);
-
-        ContatoEntity contatoEntity = objectMapper.convertValue(contatoCreateDTO, ContatoEntity.class);
-        ContatoEntity criado = contatoRepository.create(contatoEntity);
-
-        ContatoDTO contatoDTO = objectMapper.convertValue(criado, ContatoDTO.class);
-
-        return contatoDTO;
-    }
-
     public List<ContatoDTO> list(){
         return contatoRepository.list().stream()
                 .map(contato -> objectMapper.convertValue(contato, ContatoDTO.class))
                 .collect(Collectors.toList());
+
+
     }
 
-    public List<ContatoDTO> listByIdPessoa(Integer id) throws RegraDeNegocioException {
-        pessoaRepository.getIdById(id);
-        return contatoRepository.listByIdPessoa(id).stream()
+    public List<ContatoDTO> listPorIdPessoa(Integer idPessoa) throws RegraDeNegocioException {
+        pessoaRepository.getIdById(idPessoa);
+        return contatoRepository.listByIdPessoa(idPessoa).stream()
                 .map(contato -> objectMapper.convertValue(contato, ContatoDTO.class))
                 .collect(Collectors.toList());
     }
 
-    public ContatoDTO update(Integer id, ContatoCreateDTO contatoCreateDTO) throws Exception {
-        ContatoEntity contatoEntity = objectMapper.convertValue(contatoCreateDTO, ContatoEntity.class);
-        ContatoEntity atualizado = contatoRepository.update(id, contatoEntity);
-
-        ContatoDTO contatoDTO = objectMapper.convertValue(atualizado, ContatoDTO.class);
-
+    public ContatoDTO create(Integer idPessoa, ContatoCreateDTO contatoCreateDTO) throws Exception {
+        ContatoEntity contatoEntity1 = objectMapper.convertValue(contatoCreateDTO,ContatoEntity.class);
+        pessoaRepository.getIdById(idPessoa);
+        ContatoEntity contatoCriado = contatoRepository.create(idPessoa, contatoEntity1);
+        ContatoDTO contatoDTO = objectMapper.convertValue(contatoCriado,ContatoDTO.class);
         return contatoDTO;
+
+//        PessoaEntity pessoaEntity = objectMapper.convertValue(pessoaCreateDTO,PessoaEntity.class);
+//        PessoaEntity pessoaCriada  = pessoaRepository.create(pessoaEntity);
+//        PessoaDTO pessoaDTO = objectMapper.convertValue(pessoaCriada,PessoaDTO.class);
+//        return pessoaDTO;
     }
 
-    public void delete(Integer id) throws Exception {
-        contatoRepository.delete(id);
+    public ContatoDTO update(Integer idContato, ContatoCreateDTO contatoCreateDTO) throws Exception {
+        ContatoEntity contatoEntity = objectMapper.convertValue(contatoCreateDTO,ContatoEntity.class);
+        ContatoEntity contatoEntity1 = contatoRepository.update(idContato, contatoEntity);
+        ContatoDTO dto = objectMapper.convertValue(contatoEntity1,ContatoDTO.class);
+        return dto;
     }
 
+    public void delete(Integer idContato) throws Exception {
+        contatoRepository.delete(idContato);
+    }
 }
