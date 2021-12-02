4f0f45f6d5fbd5963e02116d3560e4a81372d804
Adriele Beatriz Santos <adriele.beatriz98@outlook.com>
2021-12-02 07:01:38 -0300

    atividades Scheduler 1 e 2

---
+++ Modulo 6.2/pessoaapi/src/main/java/com/dbc/pessoaapi/scheduler/MeuNovoSchedulerCron.java
@@ -0,0 +1,49 @@
+package com.dbc.pessoaapi.scheduler;
+
+import com.dbc.pessoaapi.dto.PessoaDTO;
+import com.dbc.pessoaapi.entity.PessoaEntity;
+import com.dbc.pessoaapi.repository.PessoaRepository;
+import com.dbc.pessoaapi.service.EmailService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import freemarker.template.TemplateException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class MeuNovoSchedulerCron {
+    private static final SimpleDateFormat dateFormat
+            = new SimpleDateFormat("HH:mm:ss");
+    private final PessoaRepository pessoaRepository;
+
+    private final EmailService emailService;
+    private final ObjectMapper objectMapper;
+
+    @Scheduled(cron = "0 0 0 8,18 * *", zone = "GMT-3")
+    public void meuPrimeiroScheduler() throws InterruptedException, MessagingException, TemplateException, IOException {
+//        Thread.sleep(1000);
+        log.info("{}", dateFormat.format(new Date()));
+        List<PessoaEntity> pessoaEntities = pessoaRepository.pessoaSemEndereco();
+        for(PessoaEntity pessoa : pessoaEntities){
+            System.out.println(pessoa.getIdPessoa());
+            PessoaDTO pessoaDTO = objectMapper.convertValue(pessoa, PessoaDTO.class);
+            emailService.enviarEmailComTemplatePessoaSemEndereco(pessoaDTO);
+        }
+    }
+    @Scheduled(cron = "0 44 18 * * *", zone = "GMT-3")
+    public void meuSegundoScheduler() throws InterruptedException {
+        List<PessoaEntity> todasAsPessoas  = pessoaRepository.findAll();
+        for (PessoaEntity pessoa: todasAsPessoas) {
+            emailService.enviarEmailChristmans(pessoa);
+        }
+    }
+}
---
+++ Modulo 6.2/pessoaapi/src/main/java/com/dbc/pessoaapi/service/EmailService.java
@@ -0,0 +1,74 @@
+package com.dbc.pessoaapi.service;
+
+import com.dbc.pessoaapi.controller.PessoaController;
+import com.dbc.pessoaapi.dto.PessoaDTO;
+import com.dbc.pessoaapi.dto.PessoaEnderecoDTO;
+import com.dbc.pessoaapi.entity.PessoaEntity;
+import com.dbc.pessoaapi.repository.PessoaRepository;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+    @Value("${spring.mail.username}")
+    private String remetente;
+    private final Configuration configuration;
+
+
+
+    public void enviarEmailComTemplatePessoaSemEndereco(PessoaDTO pessoaDTO) throws MessagingException, IOException, TemplateException {
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setFrom(remetente);
+        helper.setTo(pessoaDTO.getEmail());
+        helper.setSubject("email");
+        Template template = configuration.getTemplate("email-templatedelete.ftl");
+        Map<String, Object> dados = new HashMap<>();
+        dados.put("nomePessoa", pessoaDTO.getNome());
+        dados.put("nomePessoa", pessoaDTO.getNome());
+        dados.put("email", remetente);
+        String html = FreeMarkerTemplateUtils.processTemplateIntoString(template, dados);
+
+        helper.setText(html, true);
+
+        emailSender.send(mimeMessage);
+    }
+
+    public void enviarEmailChristmans(PessoaEntity pessoaEntity) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(remetente);
+        message.setTo(pessoaEntity.getEmail());
+        message.setSubject("Promoção de Natal");
+        message.setText("Olá " + pessoaEntity.getNome() + "," +
+                "\n\nSelecionamos algumas das nossas super promoções de natal na nossa plataforma especialmente para você:" +
+                "\n-Na compra de 1CD do Chitãozinho e Xororó, ganhe 1 do Milionário e José Rico." +
+                "\n-Na locação de um filme em VHS, a outra locação é grátis" +
+                "\n-Fita de Super Nitendo com 50% de desconto." +
+                "\n\nAproveite..." +
+                "\nMagazine OldSchool");
+        emailSender.send(message);
+    }
+
+}
